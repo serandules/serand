079fe902c73fa7779697bd281773d4ea15b9151a
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-07-20 06:03:30 +0530

    updating

---
+++ index.js
@@ -0,0 +1,11 @@
+var utils = require('utils');
+
+var bucket = process.env.AWS_S3_CDN_BUCKET || 'cdn.serandives.com'
+
+module.exports.index = function (id, revision, done) {
+    var s3 = utils.s3();
+    s3.getObject({
+        Bucket: bucket,
+        Key: id + '/' + revision + '/' + id + '/index.html'
+    }, done);
+};
