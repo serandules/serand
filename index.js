4870690f8bb2ff00f99fc587d434dbed299bdc29
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-10-01 00:30:54 +0530

    updating

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var log = require('logger')('serand');
 var nconf = require('nconf');
 var request = require('request');
 
-var cdn = nconf.get('cdn');
+var cdn = nconf.get('CDN');
 
 module.exports.index = function (id, revision, done) {
     var url = cdn + '/' + id + '/' + revision + '/' + id + '/index.html';
