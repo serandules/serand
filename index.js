c0a0d6cad9d30a4f8e3eb25f2605a563f2ce75a3
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-10-25 19:38:34 +0530

    updating

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ var log = require('logger')('serand');
 var nconf = require('nconf');
 var request = require('request');
 
+var utils = require('utils');
+
+var Configs = require('model-configs');
+var Clients = require('model-clients');
+
+var space = utils.space();
+
 var cdn = nconf.get('CDN');
 
 module.exports.index = function (id, revision, done) {
-    var url = cdn + '/' + id + '/' + revision + '/' + id + '/index.html';
-    request(url, function (err, res, body) {
-        done(err, body);
+  Clients.findOne({name: space}).exec(function (err, client) {
+    if (err) {
+      return done(err);
+    }
+    if (!client) {
+      return done('No client with name %s can be found.', space);
+    }
+    Configs.findOne({user: client.user, name: 'boot'}, function (err, boot) {
+      if (err) {
+        return done(err);
+      }
+      if (!boot) {
+        return done('No boot config cannot be found');
+      }
+      var url = cdn + '/' + id + '/' + revision + '/' + id + '/index.html';
+      request(url, function (err, res, body) {
+        done(err, boot.id, body);
+      });
     });
+  });
 };
 
 var Error = function (o) {
