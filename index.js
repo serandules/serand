b8c2c69a03135a5dc678cbe9eb7ee0c4c2c5d3bc
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-10-11 10:52:07 +0530

    updating

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var log = require('logger')('serand');
 var nconf = require('nconf');
+var async = require('async');
 var request = require('request');
 var mongoose = require('mongoose');
 var _ = require('lodash');
 
 var utils = require('utils');
 
-var domain = utils.domain();
-
 var cdn = nconf.get('CDN_STATICS');
 
-module.exports.index = function (id, revision, done) {
+exports.index = function (id, revision, done) {
   var url = cdn + '/' + id + '/' + revision + '/' + id + '/index.html';
   log.info('index:url', 'id:%s, url:%s', id, url);
   request(url, function (err, res, body) {
@@ -18,31 +17,51 @@ module.exports.index = function (id, revision, done) {
   });
 };
 
-module.exports.configs = function (names, done) {
-  var Clients = mongoose.model('clients');
-  Clients.findOne({name: domain}).exec(function (err, client) {
+exports.boots = function (names, done) {
+  exports.configs(['boot', 'groups', 'menus', 'aliases'].concat(names), function (err, configs) {
     if (err) {
       return done(err);
     }
-    if (!client) {
-      return done('No client with name %s can be found.', domain);
-    }
-    var Configs = mongoose.model('configs');
-    Configs.find({
-      user: client.user,
-      name: {
-        $in: names
-      }
-    }, {name: 1, value: 1}, function (err, configs) {
+    exports.configs(Object.keys(configs.menus), function (err, menus) {
       if (err) {
         return done(err);
       }
-      var o = {};
-      configs.forEach(function (config) {
-        config = config.toJSON();
-        o[config.name] = config.value;
-      });
-      done(null, o);
+      done(null, _.assign(configs, menus));
     });
+  });
+};
+
+exports.configs = function (names, done) {
+  var o = {};
+  async.each(names, function (name, eachDone) {
+    utils.client(function (err, client) {
+      if (err) {
+        return done(err);
+      }
+      utils.cached('configs:' + client.user + ':' + name, function (err, value) {
+        if (err) return eachDone(err);
+        if (value) {
+          o[name] = JSON.parse(value);
+          return eachDone();
+        }
+        var Configs = mongoose.model('configs');
+        Configs.findOne({
+          user: client.user,
+          name: name
+        }, {value: 1}, function (err, config) {
+          if (err) {
+            return eachDone(err);
+          }
+          config = utils.json(config);
+          o[name] = config.value;
+          eachDone();
+        });
+      });
+    });
+  }, function (err) {
+    if (err) {
+      return done(err);
+    }
+    done(null, o);
   });
 };
